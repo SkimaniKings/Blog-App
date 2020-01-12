632034cbdb041d0f69664f645855a29532d48571
skimanikings <kimanisimon856@gmail.com>
2020-01-12 22:43:39 +0300

    add dummy data to test for displaying of the output

--- flaskblog.py
+++ flaskblog.py
@@ -1,11 +1,30 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': ' Simon Kimani',
+        'title': 'Blog post 1',
+        'content': 'First post Content',
+        'date_posted': 'January 9, 2020'
+    },
+    {
+        'author': ' Anthony Professor',
+        'title': 'Blog post 2 ',
+        'content': 'First post Content',
+        'date_posted': 'January 9, 2020'
+    }
+]
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.htm')
+    return render_template('home.htm', posts=posts)
+
+
 @app.route('/about')
 def about():
     return render_template('about.htm')
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     app.run(debug=True)
