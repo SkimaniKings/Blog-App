d42e77c929b6b8c21e85eea342fe2d4c8e68cc5a
skimanikings <kimanisimon856@gmail.com>
2020-01-13 15:09:36 +0300

    create a repr function that displays output for an created account

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template,url_for,flash,redirect
-from forms import RegistrationForm,LoginForm
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '6f537a0de4d614ba210441a2049f30d0'
@@ -7,12 +7,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20),nullable=False, default='default.jpg')
-    password = db.Column(db.string(60), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
 
 posts = [
     {
@@ -37,18 +40,22 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.htm')
-@app.route('/register', methods= ['GET','POST'])
+
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'You have successfully created an account for {form.username.data}!','success')
+        flash(
+            f'You have successfully created an account for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.htm', form=form)
-@app.route('/login', methods= ['GET','POST'])
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form =LoginForm()
+    form = LoginForm()
     return render_template('login.htm', form=form)
-      
 
 
 if __name__ == "__main__":
