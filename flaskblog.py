3dab2e71e246c625c64740bb054f3e69f68591f3
skimanikings <kimanisimon856@gmail.com>
2020-01-13 12:32:58 +0300

    create an sql alchemy database instance

--- flaskblog.py
+++ flaskblog.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '6f537a0de4d614ba210441a2049f30d0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+db = SQLAlchemy(app)
+
 posts = [
     {
         'author': ' Simon Kimani',
