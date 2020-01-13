66548197f4c947257aa752448434dbac1ce0a0a2
skimanikings <kimanisimon856@gmail.com>
2020-01-13 15:22:49 +0300

    fixed errors on the db

--- flaskblog.py
+++ flaskblog.py
@@ -11,10 +11,18 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False,
-                           default='default.jpg')
+    email = db.Column(db.String(120), unique=True,  nullable=False)
+    image_file = db.Column(db.String(20), unique=True,nullable=False)
     password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False)
 
 
 posts = [
