fb1caec7ac38d68617ba144676e934f0cc053826
skimanikings <kimanisimon856@gmail.com>
2020-01-13 12:52:14 +0300

    add a paswords column

--- flaskblog.py
+++ flaskblog.py
@@ -12,6 +12,7 @@ class User(db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20),nullable=False, default='default.jpg')
+    password = db.Column(db.string(60), nullable=False)
 
 posts = [
     {
