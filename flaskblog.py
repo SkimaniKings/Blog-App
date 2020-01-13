f43d07aaa909010eea3805bba5b150b587fd59b2
skimanikings <kimanisimon856@gmail.com>
2020-01-13 12:48:03 +0300

    add an emeil column

--- flaskblog.py
+++ flaskblog.py
@@ -9,7 +9,8 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20) unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
 posts = [
     {
