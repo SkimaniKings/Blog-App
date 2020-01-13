bfac766180cbff82d1cbf544516be2b12df006db
skimanikings <kimanisimon856@gmail.com>
2020-01-13 03:37:18 +0300

    add a password to our account creation page

--- forms.py
+++ forms.py
@@ -4,4 +4,4 @@ from wtforms.validators import DataRequired,Length,Email
 class RegistrationForm(FlaskForm):
     Username = StringField('Username' ,validators=[DataRequired(),Length(min=2,max = 20)])
     Email = StringField('Email',validators=[DataRequired(), Email()]) 
-    
+    Password = PasswordField('Password',validators=DataRequired()
