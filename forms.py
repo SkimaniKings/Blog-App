c9d406f6d70084c548d91bbf8ed04c60cca4f564
skimanikings <kimanisimon856@gmail.com>
2020-01-13 03:40:48 +0300

    import equalto module

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField
-from wtforms.validators import DataRequired,Length,Email
+from wtforms.validators import DataRequired,Length,Email,EqualTo
 class RegistrationForm(FlaskForm):
     Username = StringField('Username' ,validators=[DataRequired(),Length(min=2,max = 20)])
     Email = StringField('Email',validators=[DataRequired(), Email()]) 
