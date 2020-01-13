e4d3e5773a02eb4470cb1ae7f440bd063d132250
skimanikings <kimanisimon856@gmail.com>
2020-01-13 03:40:05 +0300

    create a confirm passord

--- forms.py
+++ forms.py
@@ -4,4 +4,5 @@ from wtforms.validators import DataRequired,Length,Email
 class RegistrationForm(FlaskForm):
     Username = StringField('Username' ,validators=[DataRequired(),Length(min=2,max = 20)])
     Email = StringField('Email',validators=[DataRequired(), Email()]) 
-    Password = PasswordField('Password',validators=DataRequired()
+    Password = PasswordField('Password',validators=DataRequired())
+    confirm_password = PasswordField('Confirm Password', validators=DataRequired())
