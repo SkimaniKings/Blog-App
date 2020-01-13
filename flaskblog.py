97c6b556420c2afb9dd2bf11706f4ea0561708be
skimanikings <kimanisimon856@gmail.com>
2020-01-13 12:29:39 +0300

    specifyuri for the database

--- flaskblog.py
+++ flaskblog.py
@@ -3,6 +3,7 @@ from forms import RegistrationForm,LoginForm
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '6f537a0de4d614ba210441a2049f30d0'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 posts = [
     {
