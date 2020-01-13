5e2bd384f00676e3aaefa130a60be151a2b5a7d5
skimanikings <kimanisimon856@gmail.com>
2020-01-13 03:49:39 +0300

    import the submit fiels and create a submitt button

--- forms.py
+++ forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField
+from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired,Length,Email,EqualTo
 class RegistrationForm(FlaskForm):
     Username = StringField('Username' ,validators=[DataRequired(),Length(min=2,max = 20)])
     Email = StringField('Email',validators=[DataRequired(), Email()]) 
     Password = PasswordField('Password',validators=DataRequired())
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('Password')])
+    Submit = SubmitField('Sign_Up')
