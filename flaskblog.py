4140d1ebe4b85a5a3ebfdb31dc31b559bf9e57b2
skimanikings <kimanisimon856@gmail.com>
2020-01-13 15:26:09 +0300

    Create a content column on the posts

--- flaskblog.py
+++ flaskblog.py
@@ -24,6 +24,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
 
 
 posts = [
