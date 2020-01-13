0583093123cdd47a26665ef1359f880664137029
skimanikings <kimanisimon856@gmail.com>
2020-01-13 15:42:54 +0300

    create a posts relationship and pass in backref and lazy arguments

--- flaskblog.py
+++ flaskblog.py
@@ -15,6 +15,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True,  nullable=False)
     image_file = db.Column(db.String(20), unique=True,nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    posts = db.realationship('Post', backref ='author',lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}','{self.email}', '{self.image_file}')"
@@ -25,6 +26,9 @@ class Post(db.Model):
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default = datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
+    
+def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
 
 
 posts = [
