92b0f625902fd456c2f4a2552d218088f2d8d036
skimanikings <kimanisimon856@gmail.com>
2020-01-13 03:21:10 +0300

    create a username and add validors to limit length and make it required

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired,Length
 class RegistrationForm(FlaskForm):
-    username = StringField('username' validators=[])
+    username = StringField('username' ,validators=[DataRequired(),Length(min=2,max = 20)])
     
