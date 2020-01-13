1139899c269566d1a6d4ffa973aa1141ca37be8d
skimanikings <kimanisimon856@gmail.com>
2020-01-13 10:12:39 +0300

    install sqlalchemy and import

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,url_for,flash,redirect
 from forms import RegistrationForm,LoginForm
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '6f537a0de4d614ba210441a2049f30d0'
 
@@ -33,7 +34,7 @@ def register():
         flash(f'You have successfully created an account for {form.username.data}!','success')
         return redirect(url_for('home'))
     return render_template('register.htm', form=form)
-@app.route('/login')
+@app.route('/login', methods= ['GET','POST'])
 def login():
     form =LoginForm()
     return render_template('login.htm', form=form)
