3eaacb10d4019fb76ee45f873ee130cffbf77aee
skimanikings <kimanisimon856@gmail.com>
2020-01-14 08:47:24 +0300

    create an init file

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,14 @@
-from datetime import datetime
+
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '6f537a0de4d614ba210441a2049f30d0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
 db = SQLAlchemy(app)
 
-
+from models import User, Post
 
 posts = [
     {
--- models.py
+++ models.py
@@ -1,3 +1,6 @@
+from __main__ import db
+from datetime import datetime
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
