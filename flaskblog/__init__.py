28fa7feae34c1c3d0936f59d08748124c8134676
skimanikings <kimanisimon856@gmail.com>
2020-01-14 09:46:36 +0300

    import the flash bcryption file

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '6f537a0de4d614ba210441a2049f30d0'
