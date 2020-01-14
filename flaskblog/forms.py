ea259ef25d12997e63e7a19aa09f65409717dd91
skimanikings <kimanisimon856@gmail.com>
2020-01-14 10:45:16 +0300

    import user to the forms and create a variable to hold the usernames in the databse

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -12,6 +12,13 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+     
+     def validate_username(self, username):
+         
+         user=User.query.filter_by(username=username.data)
+         if True:
+             raise ValidationError('Validation Message')
+         
 
 
 class LoginForm(FlaskForm):
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -34,13 +34,13 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password= bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.date, email=form.email.data, password=hashed_password)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         
         flash(
             f'You have successfully created an account for {form.username.data}! You can now log in to your account','success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     return render_template('register.htm', form=form)
 
 
