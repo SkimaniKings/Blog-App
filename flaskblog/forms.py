c0f4bb11b7cf8eeeb96fcccc76c1a1ca1fabd936
skimanikings <kimanisimon856@gmail.com>
2020-01-15 01:36:01 +0300

    import text area field and create form for posting

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField,FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField,TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
@@ -57,5 +57,6 @@ class UpdateForm(FlaskForm):
             email=User.query.filter_by(email=email.data).first()
             if email:
                 raise ValidationError('Email is already taken. Choose a different one')
-
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()] )
 
