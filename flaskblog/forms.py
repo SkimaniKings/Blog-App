d00cfbde238b55ea011322ddb124e6d312250d58
skimanikings <kimanisimon856@gmail.com>
2020-01-15 01:25:09 +0300

    make changes on how to update profile picture

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -40,8 +40,8 @@ class UpdateForm(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    picture = FileField('Update Profile Picture',validators=[FileAllowed(['jpg','png','jpeg'])])
-    
+    # picture = FileField('Update Profile Picture',validators=[FileAllowed(['jpg','png','jpeg'])])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
      
     def validate_username(self, username):
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,3 +1,5 @@
+import os
+import secrets
 from flask import render_template, url_for, flash, redirect,request
 from flaskblog import app, db, bcrypt
 from flaskblog.models import User, Post
@@ -70,9 +72,27 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 @app.route('/profile', methods=['GET', 'POST'])
+
+# def save_picture(form_picture):
+#     random_hex = secrets.token_hex(8)
+#     _, f_ext = os.path.splitext(form_picture.filename)
+#     picture_fn = random_hex + f_ext
+#     picture_path = os.path.join(app.root_path, 'static/pics', picture_fn)
+
+#     output_size = (125, 125)
+#     i = Image.open(form_picture)
+#     i.thumbnail(output_size)
+#     i.save(picture_path)
+
+#     return picture_fn
+        
 def profile():
     form = UpdateForm()
     if form.validate_on_submit():
+        # if form.picture.data:
+        #     picture_file = save_picture(form.picture.data)
+        #     current_user.image_file = picture_file
+            
         current_user.username= form.username.data
         current_user.email= form.email.data
         db.session.commit()
