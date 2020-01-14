77ea5b3a09eba8d088d34e35e2b1667d84710477
skimanikings <kimanisimon856@gmail.com>
2020-01-14 12:59:17 +0300

    create a logout route function

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.models import User, Post
 from flaskblog.forms import RegistrationForm, LoginForm
-from flask_login import login_user,current_user
+from flask_login import login_user,current_user,logout_user
 
 
 posts = [
@@ -32,6 +32,8 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password= bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -47,6 +49,9 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+         
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -59,3 +64,5 @@ def login():
         
         
     return render_template('login.htm', form=form)
+@app.route('logout')
+def logout():
