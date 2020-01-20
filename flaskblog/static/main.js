c8c8f5ec92fbad8956302dcb87ce0585f017c303
skimanikings <kimanisimon856@gmail.com>
2020-01-20 07:35:25 +0300

    added a dislike an like button that actually work

---
+++ flaskblog/static/main.js
@@ -0,0 +1,21 @@
+
+// $(document).ready(function(){
+//     let numberOfLikes = 0
+//     $("#like").click(function(){
+//         numberOfLikes = +1
+//         document.getElementById("like").innerHTML = numberOfLikes;
+
+//     });
+
+// });
+
+let click = 0;
+let clicks = 0;
+function onClick() {
+    click += 1;
+    document.getElementById("click").innerHTML = click;
+}
+function onClicks() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+}
